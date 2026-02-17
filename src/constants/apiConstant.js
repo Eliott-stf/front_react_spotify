ac23b961566da439381b98b96cf5eae62308b8e7
ES_PREPA_DEV <setifeliott@gmail.com>
2026-02-17 09:19:48 +0100

    add api constants

---
+++ src/constants/apiConstant.js
@@ -0,0 +1,32 @@
+/**
+ * CONSTANTES DE L'API
+ * ce ficher centralise toute les URL de l'api Symfony
+ * l'avantage: Modifier l'url de base en un seul endroit  
+*/
+
+//URL racine du serveur backend 
+export const API_ROOT = "http://localhost:8088";
+
+//L'URL de base de l'API Platform
+export const API_URL = `${API_ROOT}/api`
+
+/**
+ * URL DES RESSOURCES STATIQUES
+*/
+
+//Images générales (logo, etc)
+export const IMAGE_URL = `${API_ROOT}/images`;
+
+//Images des avatars utilisateurs
+export const AVATAR_URL = `${IMAGE_URL}/avatars`
+
+//Image de cover des albums
+export const COVER_URL = `${API_ROOT}/upload/images/albums`;
+
+//Photos des artistes 
+export const ARTIST_URL = `${API_ROOT}/upload/images/artists`;
+
+//Fichiers audio MP3
+export const MUSIC_URL = `${API_ROOT}/upload/files/music`;
+
+
