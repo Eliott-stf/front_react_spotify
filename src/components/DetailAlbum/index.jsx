e4288cba576c490ee7ac47faf1a35cfbbd6de3b4
ES_PREPA_DEV <setifeliott@gmail.com>
2026-02-19 15:52:55 +0100

    add album detail on store

---
+++ src/components/DetailAlbum/index.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { fetchAlbumDetail } from '../../store/album/albumSlice';
+
+const DetailAlbum = () => {
+
+
+    return (
+        <div>DetailAlbum</div>
+    )
+}
+
+export default DetailAlbum
--- src/screens/OnlineScreens/Detail.jsx
+++ src/screens/OnlineScreens/Detail.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchAlbumDetail } from '../../store/album/albumSlice';
+import { useParams } from 'react-router-dom';
+import selectAlbumData from '../../store/album/albumSelector';
+import PageLoader from '../../components/Loader/PageLoader';
+import DetailAlbum from '../../components/DetailAlbum';
 
 const Detail = () => {
+
+  //const pour récupérer l'id 
+  const params = useParams();
+
+  const { id } = params;
+
+  //on récupère le hook dispatch pour pouvoir executer les actions du slice
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // on dispatch la méthode fetchAlbumDetail pour récupérer les albums en bdd
+    dispatch(fetchAlbumDetail(id));
+  }, [dispatch, id])
+
+  //on récupère les données des détails de l'albums et le loading avec useSelector
+  const { loading, albumDetail } = useSelector(selectAlbumData);
+
   return (
-    <div>Detail</div>
+    loading ? <PageLoader /> : <DetailAlbum dataAlbum={albumDetail} />
   )
 }
 
